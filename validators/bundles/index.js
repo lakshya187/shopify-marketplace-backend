fc2cd46078a7c2311ce83ebb0e8e2473c5aaf07e
lakshyaGiftkart <lakshya@giftkart.ai>
2024-11-21 22:16:43 +0530

    modifications in bundle creation logic and validation

--- validators/bundles/index.js
+++ validators/bundles/index.js
@@ -11,7 +11,7 @@ export const validateCreateBundle = Joi.object({
     "string.base": "Description must be a string.",
     "string.max": "Description cannot exceed 500 characters.",
   }),
-  product_ids: Joi.array()
+  productIds: Joi.array()
     .items(Joi.string().required())
     .min(1)
     .required()
@@ -36,4 +36,42 @@ export const validateCreateBundle = Joi.object({
   metadata: Joi.object().optional().messages({
     "object.base": "Metadata must be an object.",
   }),
+  costOfGoods: Joi.number().min(0).max(100).optional().messages({
+    "number.base": "Cost must be a number.",
+    "number.min": "Cost cannot be less than 0.",
+  }),
+  isOnSale: Joi.boolean().optional(),
+  height: Joi.number().optional({
+    "number.base": "Height must be a number.",
+    "number.min": "Height cannot be less than 0.",
+  }),
+  width: Joi.number().optional({
+    "number.base": "Width must be a number.",
+    "number.min": "Width cannot be less than 0.",
+  }),
+  length: Joi.number().optional({
+    "number.base": "Length must be a number.",
+    "number.min": "Length cannot be less than 0.",
+  }),
+  weight: Joi.number().optional({
+    "number.base": "Weight must be a number.",
+    "number.min": "Weight cannot be less than 0.",
+  }),
+  coverImage: Joi.string().uri().optional().messages({
+    "string.base": "Cover Image must be a string.",
+    "string.empty": "Cover Image is required.",
+    "string.uri": "Cover Image must be a valid URL.",
+  }),
+  images: Joi.array().items(
+    Joi.object({
+      src: Joi.string().uri().required().messages({
+        "string.base": "Image source must be a string.",
+        "string.uri": "Image source must be a valid URL.",
+        "any.required": "Image source is required.",
+      }),
+      altText: Joi.string().optional().messages({
+        "string.base": "Alt Text must be a string.",
+      }),
+    }),
+  ),
 });
