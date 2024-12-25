8673aa0f7ea49f26c40ba8900e2abba7d2fff3c3
lakshyaGiftkart <lakshya@giftkart.ai>
2024-12-25 20:30:28 +0530

    access token scope updated

--- constants/shopify.js
+++ constants/shopify.js
@@ -4,5 +4,5 @@
 //     : ""
 // }read_orders, write_orders, read_customers, write_customers, read_discounts, write_discounts, write_script_tags, read_draft_orders, write_draft_orders, read_fulfillments, read_shipping`;
 
-export const BASIC_AUTH_SCOPE = `read_products, write_products, read_inventory, read_orders, write_orders, write_files, write_draft_orders, read_customers, customer_write_customers, write_publications, read_locations, write_inventory, write_inventory, write_discounts, read_discounts, read_assigned_fulfillment_orders, read_merchant_managed_fulfillment_orders, read_third_party_fulfillment_orders, write_fulfillments, write_assigned_fulfillment_orders, write_merchant_managed_fulfillment_orders, fulfill_and_ship_orders, write_third_party_fulfillment_orders`;
+export const BASIC_AUTH_SCOPE = `read_products, write_products, read_inventory, read_orders, write_orders, write_files, write_draft_orders, read_customers, customer_write_customers, write_publications, read_locations, write_inventory, write_inventory, write_discounts, read_discounts, read_assigned_fulfillment_orders, read_merchant_managed_fulfillment_orders, read_third_party_fulfillment_orders, write_fulfillments, write_assigned_fulfillment_orders, write_merchant_managed_fulfillment_orders, write_third_party_fulfillment_orders`;
 export const INTERNAL_STORES = [process.env.INTERNAL_STORE];
